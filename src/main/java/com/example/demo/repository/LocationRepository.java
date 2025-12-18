a94d4aac7bdfc08257f3dc9916713e884425b16f
bala-amypo <bala@amypo.in>
2025-12-18 09:28:29 +0000

    User commit

--- src/main/java/com/example/demo/repository/LocationRepository.java
+++ src/main/java/com/example/demo/repository/LocationRepository.java
@@ -2,7 +2,6 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Location;
-import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
 }
--- src/main/java/com/example/demo/repository/ShipmentRepository.java
+++ src/main/java/com/example/demo/repository/ShipmentRepository.java
@@ -2,7 +2,7 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Shipment;
-import java.util.Optional;
+import java.util.List;
 
 public interface ShipmentRepository extends JpaRepository<Shipment,Long>{
     List<Shipment>findByVehicleId(Long vehicleId);
