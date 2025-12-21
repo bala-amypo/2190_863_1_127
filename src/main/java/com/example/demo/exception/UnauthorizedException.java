d801c55698a1584f84ce4faacd7a6cbbbd09cce1
bala-amypo <bala@amypo.in>
2025-12-21 12:26:47 +0000

    User commit

--- src/main/java/com/example/demo/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-// UnauthorizedException.java
-package com.example.demo.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
