f0e8dc6f080beada209ca966a229a4b245e711eb
bala-amypo <bala@amypo.in>
2025-12-22 08:00:19 +0000

    User commit

--- src/main/java/com/example/demo/controller/ComplaintController.java
+++ src/main/java/com/example/demo/controller/ComplaintController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.entity.Complaint;
-import com.example.demo.entity.User;
 import com.example.demo.service.ComplaintService;
 import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.*;
