a2695c793c9d0f01f755755575fb62d2f92b8c79
bala-amypo <bala@amypo.in>
2025-12-22 08:02:56 +0000

    User commit

--- src/main/java/com/example/demo/controller/ComplaintController.java
+++ src/main/java/com/example/demo/controller/ComplaintController.java
@@ -2,8 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Complaint;
 import com.example.demo.service.ComplaintService;
-import com.example.demo.service.UserService;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -11,37 +11,37 @@ import java.util.List;
 public class ComplaintController {
 
     private final ComplaintService service;
-    private final UserService userService;
 
-    public ComplaintController(ComplaintService service, UserService userService) {
+    public ComplaintController(ComplaintService service) {
         this.service = service;
-        this.userService = userService;
     }
 
-    @PostMapping("/submit/{email}")
-    public Complaint submit(@PathVariable String email,
-                            @RequestBody Complaint complaint) {
-        User user = userService.findByEmail(email);
-        complaint.setCustomer(user);
+
+    @PostMapping
+    public Complaint submit(@RequestBody Complaint complaint) {
         return service.submitComplaint(complaint);
     }
 
-    @GetMapping("/user/{email}")
-    public List<Complaint> byUser(@PathVariable String email) {
-        return service.getComplaintsForUser(userService.findByEmail(email));
+
+    @GetMapping
+    public List<Complaint> getAll() {
+        return service.getAllComplaints();
     }
+
 
     @GetMapping("/prioritized")
     public List<Complaint> prioritized() {
         return service.getPrioritizedComplaints();
     }
 
+  
     @PutMapping("/{id}/status")
-    public Complaint updateComplaintStatus(@PathVariable Long id,@RequestParam Complaint.Status status) {
+    public Complaint updateComplaintStatus(
+            @PathVariable Long id,
+            @RequestParam Complaint.Status status) {
 
-    Complaint complaint = service.getComplaintById(id);
-    complaint.setStatus(status);
-    return service.updateComplaint(complaint);
-}
-
+        Complaint complaint = service.getComplaintById(id);
+        complaint.setStatus(status);
+        return service.updateComplaint(complaint);
+    }
 }
