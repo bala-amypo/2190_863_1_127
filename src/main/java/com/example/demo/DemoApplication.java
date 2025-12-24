0f73396a6bdc865c589c31de53b61f98b6723be8
bala-amypo <bala@amypo.in>
2025-12-24 04:10:56 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import com.example.demo.servlet.SimpleEchoServlet;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+    
+    @Bean
+    public ServletRegistrationBean<SimpleEchoServlet> simpleEchoServlet() {
+        ServletRegistrationBean<SimpleEchoServlet> bean = new ServletRegistrationBean<>();
+        bean.setServlet(new SimpleEchoServlet());
+        bean.addUrlMappings("/simple-echo");
+        return bean;
+    }
 }
