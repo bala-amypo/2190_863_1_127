3a9cf7225d1ab4adce8481d4e9d5929e2d47cc9f
bala-amypo <bala@amypo.in>
2025-12-25 13:19:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/PriorityRuleController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.PriorityRule;
+import com.example.demo.service.PriorityRuleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rules")
+public class PriorityRuleController {
+    private final PriorityRuleService priorityRuleService;
+    
+    public PriorityRuleController(PriorityRuleService priorityRuleService) {
+        this.priorityRuleService = priorityRuleService;
+    }
+    
+    @GetMapping("/all")
+    public ResponseEntity<List<PriorityRule>> getAllRules() {
+        List<PriorityRule> rules = priorityRuleService.getActiveRules();
+        return ResponseEntity.ok(rules);
+    }
+}
