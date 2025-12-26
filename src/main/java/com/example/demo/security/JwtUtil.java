cf4688ebb2b6e49d2d6f33ddb9c9e759b3a75000
bala-amypo <bala@amypo.in>
2025-12-26 06:47:25 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,5 +1,8 @@
 package com.example.demo.security;
 
+import org.springframework.stereotype.Component;
+
+@Component   // 🔥 THIS IS THE FIX
 public class JwtUtil {
 
     public String extractRole(String token) {
